7f3083b4bd0d8e374833bafdec296bbd75feb446
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-26 08:40:02 +0530

    Create VariablesInJava.java

---
+++ VariablesInJava.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.lang.*;
+
+public class VariablesInJava
+{
+	public static void main(String[] args) {
+	    int a = 10 ;    # variable name is a of integer type decalred 
+      System.out.println(a);
+		
+		
+	}
+}
+
+/*
+                   Variables 
+                   ---------
+                   
+-> As we study in maths let's assume we have given some  
+   rectangle with its  length & breadth with name l & b 
+   So these a & b are our variables or containers that will  
+   store our value in the memory . 
+   
+-> In english variables means anything whose value can be change.
+
+-> a=10 , b = 5 ;   # variables 
+-> find its perimeter of this rect ? 
+
+->  2 * (a+b)
+
+                literals
+                --------
+
+-> so in programming this 2 is a literal because it's value is 
+   fixed or same or universal it doesn't depend on a & b values .
+
+
+-> So literal also of many types like  1,2,3,4,5.. these are number 
+  literals .
+  
+->'a', 'b' , 'c' ... these are character literals .
+
+
+-> So it math we declar variables like this a =10 , b=20 .. and so on..
+-> but in programming like java we define variables differently .
+   
+   first we writes it's datatype  then its' name 
+   
+   example-:   int a = 10 ;
+               String b = "lalit" ;
+               float c = 22.3f ;
+               
+
+
+                                  Identifiers
+                                  -----------
+                                  
+-> So the name of these varaibales , class , function ,.. these are 
+   known as Identifiers in java. 
+   
+   
+                                Memory 
+                                ------ 
+                                
+->So in java any infomation,variables that our program contains 
+  store in a memory .
+  
+-> Memory have a unique address or small blocks just like our house
+  number in real scenrio . 
+  
+-> so these variables points this memory address where our data is 
+  stored  & when we call these variables we will get our data stored 
+  in the memory .
+  
+-> memory will occupy by our variables based on the datatype of variables
+  
+
+
+*/
