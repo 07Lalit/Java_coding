e66045b62738f25fce4531348b701c182f54ae9f
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-26 08:07:04 +0530

    javabasics_boilerPlatecode_&_output_in_java

---
+++ javabasics_boilerPlatecode_&_output.java
@@ -0,0 +1,27 @@
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.print("Hello Lalit");
+		System.out.println(" Hello Duniya");
+		System.out.println("1 2 3 4 5");
+		System.out.println("hello world \n hello world \n");
+	}
+}
+
+/* 
+So What we understand from this is :
+
+1) java boiler code or basic template .
+
+2) print  -> it will print everything in the quotes & doesn't 
+           change the cursor postion to the next line or newline .
+           
+3) println -> After printing it will shift the cursor to the 
+            newline .
+            
+4) "\n"   -> It is a escape sequence we write it inside the 
+           quotes to change the cursor position to the  
+           next line  explicitly if needed .
+           
+
+*/
