132a1aa71c494ba5bd72b959d61569c5fc721694
Lalit <90924706+07Lalit@users.noreply.github.com>
2024-12-26 08:14:04 +0530

    Create OutputBasedPrintPattern.java

---
+++ OutputBasedPrintPattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+public class OutputBasedPrintPattern
+{
+	public static void main(String[] args) {
+	    System.out.println("* * * * *");
+		System.out.println("* * * *");
+		System.out.println("* * *");
+		System.out.println("* *");
+		System.out.println("*");
+		
+	}
+}
+
+/* 
+In this code  I have print the start pattern using a simple println() function but in 
+future concepts we will do the same using loops.
+*/
